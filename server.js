ee274ac798da0fc3f328473e5ad2c82b59297ae3
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-18 21:21:10 +0100

    first server created with signin validation

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'ube',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Mike',
+            email: 'mike@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
     res.send('this is working');
 })
+
+app.post('/signin', (req, res) => {
+    if( req.body.email === database.users[0].email &&  req.body.password === database.users[0].password) {
+        res.json('signing in2');
+    } else {
+        res.status(400).json('error logging in')
+    }
+    
+    
+
+})
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
+
+
+/*
+res = this is working
+signin --> POST = success or fail/ why post? sending a password to make it secure
+register --> POST = new user
+profile/:userId --> GET = user
+image --> PUT --> user
+
+*/
