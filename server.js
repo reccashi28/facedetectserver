064c089812682319928136e7cd374cb8b95ea2c3
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-19 19:19:47 +0100

    adding bcrypt for hashing pws

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,21 +25,76 @@ const database = {
         }
     ]
 }
-
+//send get request to the server
 app.get('/', (req, res) => {
-    res.send('this is working');
+    res.send(database.users);
 })
 
+//signin
 app.post('/signin', (req, res) => {
     if( req.body.email === database.users[0].email &&  req.body.password === database.users[0].password) {
         res.json('signing in2');
     } else {
         res.status(400).json('error logging in')
     }
-    
-    
 
 })
+
+app.post('/register', (req, res) => {
+
+    const { email, name, password} = req.body;
+
+    bcrypt.hash(password, null, null, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+    });
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+
+    })
+
+    res.json(database.users[database.users.length -1]); 
+});
+
+
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach ( user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    });
+    if(!found) {
+            res.status(404).json('no such user');
+    }
+
+})
+
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+
+    database.users.forEach ( user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    });
+    if(!found) {
+            res.status(404).json('no such user');
+    }
+})
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
