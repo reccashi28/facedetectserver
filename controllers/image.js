5bab604eb9d82b775741421578e6e69efb5f1618
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-29 14:17:38 +0100

    installing clarifai on server to hid api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App( {
+    apiKey: '05801666535849c18dcb0813448cb880'
+  });
+const handleAPICall = (req, res ) => {
+    app.models
+    .predict( 'c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    .then( data => {
+        res.json(data)
+    })
+    .catch( err => res.status(400).json('unable to work with api'))
+}
+
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     let found = false;
