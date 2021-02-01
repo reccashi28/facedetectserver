c39dcc3ce74d118d518c6f060d14ad768275101f
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-02-01 21:59:22 +0100

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-shaped-78131',
       user : 'wilhelmmeyer',
       password : 'Celica18ts',
       database : 'smart-brain'
